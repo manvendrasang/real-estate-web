e22456992dcec92057b5b18ce8eebb3143524ac2
Manvendra <manvendrasang@gmail.com>
2025-12-28 14:10:40 +0530

    db connection done and configuration done

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('API server is running on PORT 3000');
+});
+
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
 });
