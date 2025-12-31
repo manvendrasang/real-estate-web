3d498cbfae0b1fb9419f6d5256adcba5e845c25c
Manvendra <manvendrasang@gmail.com>
2025-12-31 17:53:52 +0530

    sign-up page frontend done

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'> 
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id="username" />
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id="email" />
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id="password" />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-85 transition duration-300 disabled:opacity-50'>
+          Create Account
+        </button>
+      </form>
+
+      <div className=''>
+        <p>Have an account?</p>
+        <span className='text-blue-700'>Sign in</span>
+      </div>
+    </div>
   )
 }
 
